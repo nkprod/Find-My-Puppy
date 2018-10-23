32739c9b48f40dff2561a85b699f3afc94717446
Nurlybek <nurlybek@nurlybeks-macbook-pro.local>
2018-10-23 15:56:21 -0400

    Initial Commit

---
+++ DogBreedRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogBreedRecognizer
+//
+//  Created by Nurlybek on 10/23/18.
+//  Copyright © 2018 Nurlybek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
